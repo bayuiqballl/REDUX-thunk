6973c0a6a0c8f0fa1c7d6751830dd93e058345c5
bayuiqballl <bayuiqballl13@gmail.com>
2020-04-22 09:10:16 +0700

    add logger

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./Reducers/";
-
+import logger from "redux-logger";
 // const store = ;
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+export default createStore(rootReducer, applyMiddleware(thunk, logger));
